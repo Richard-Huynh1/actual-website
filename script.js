6d07b2cd6a34fe665775990665aec1d6b70b1254
Richard Huynh <emailofrichardh@gmail.com>
2025-04-21 17:31:48 -0400

    Finished collapsibles and extra styling

--- script.js
+++ script.js
@@ -25,3 +25,19 @@ const plusSlides = (n) => {
 const currentSlide = (n) => {
   showSlides(slideIndex = n);
 }
+
+
+
+const collapsibles = document.getElementsByClassName("collapsible");
+
+for (let i = 0; i < collapsibles.length; i++) {
+  collapsibles[i].addEventListener("click", function() {
+    this.classList.toggle("collapsible-active");
+    const content = this.nextElementSibling;
+    if (content.style.display == "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
