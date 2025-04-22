feffb291fbb521d3abf717098c1826ab5b7225fb
Richard Huynh <emailofrichardh@gmail.com>
2025-04-22 08:29:46 -0400

    Proofread and modified code

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-let slideIndex = 1;
+let slideIndex = 0;
 
 const showSlides = (n) => {
-  let i;
   const slides = document.getElementsByClassName("slide");
   const dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
+  if (n > slides.length - 1) {slideIndex = 0}
+  if (n < 0) {slideIndex = slides.length - 1}
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
+  for (let i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex].style.display = "block";
+  dots[slideIndex].className += " active";
 }
 
 showSlides(slideIndex);
@@ -31,9 +30,9 @@ const currentSlide = (n) => {
 const collapsibles = document.getElementsByClassName("collapsible");
 
 for (let i = 0; i < collapsibles.length; i++) {
-  collapsibles[i].addEventListener("click", function() {
-    this.classList.toggle("collapsible-active");
-    const content = this.nextElementSibling;
+  collapsibles[i].addEventListener("click", () => {
+    collapsibles[i].classList.toggle("collapsible-active");
+    const content = collapsibles[i].nextElementSibling;
     if (content.style.display == "block") {
       content.style.display = "none";
     } else {
